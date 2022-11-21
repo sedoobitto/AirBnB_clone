55f1bb21e7366d3e781078757a2f69fbb5866e50
Sedoo Bitto <sedoobitto@gmail.com>
2022-11-21 06:00:07 +0000

    Update state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class State.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+
+    name = ""
