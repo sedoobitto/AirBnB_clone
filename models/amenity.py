a7f49eec44ff5bd3a7c6d681aa08d39b2572ab45
Sedoo Bitto <sedoobitto@gmail.com>
2022-11-21 05:51:54 +0000

    Update amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class Amenity.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel"""
+
+    name = ""
